060bcb1f6083dc27c3dde9c9a920ab7cd649cfe9
DownrightNifty <51222679+DownrightNifty@users.noreply.github.com>
2023-11-20 16:16:32 -0600

    Include libxml2 and example of how to use it

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,57 @@
 #include <jni.h>
 #include <string>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_myapplication_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+#include <stdio.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+
+#include <android/log.h>
+
+// redefine printf to use the android log
+#define printf(...) __android_log_print(ANDROID_LOG_DEBUG, "native-lib", __VA_ARGS__)
+
+// taken from http://xmlsoft.org/examples/#tree1.c
+void print_element_names(xmlNode * a_node)
+{
+    xmlNode *cur_node = NULL;
+
+    for (cur_node = a_node; cur_node; cur_node = cur_node->next) {
+        if (cur_node->type == XML_ELEMENT_NODE) {
+            printf("node type: Element, name: %s\n", cur_node->name);
+        }
+
+        print_element_names(cur_node->children);
+    }
 }
 
+/*
+ * Parses the XML document at the provided path, and returns a string indicating whether or not
+ * parsing was successful. Prints all nodes to the Android log.
+*/
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_myapplication_MainActivity_parseXML(
+        JNIEnv* env,
+        jobject, /* this */
+        jstring _xmlPath) {
+
+    // convert Java string to C++ string
+    std::string xmlPath(env->GetStringUTFChars(_xmlPath, 0));
+
+    // parse the xml document
+    xmlDoc *doc = NULL;
+    xmlNode *root_element = NULL;
+
+    doc = xmlReadFile(xmlPath.c_str(), NULL, 0);
+
+    if (doc == NULL) {
+        return env->NewStringUTF("could not parse");
+    }
+
+    root_element = xmlDocGetRootElement(doc);
+    print_element_names(root_element);
+    xmlFreeDoc(doc);
+    xmlCleanupParser();
+
+    return env->NewStringUTF("parsed, see logcat output");
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,10 +2,20 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.myapplication.databinding.ActivityMainBinding;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,19 +28,30 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // BOILERPLATE: initialize stuff
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        TextView textView = binding.sampleText;
 
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+        // appDataDir will be something like "/data/user/0/com.example.myapplication/files"
+        String appDataDir = this.getApplicationContext().getFilesDir().toString();
+        String xmlPath = appDataDir + "/customer_sales.xml";
+
+        // BOILERPLATE: copy from assets/customer_sales.xml to <appDataDir>/customer_sales.xml
+        AssetManager am = this.getAssets();
+        InputStream is; try { is = am.open("customer_sales.xml"); } catch (IOException e) { throw new RuntimeException(e); }
+        Scanner scanner = new Scanner(is).useDelimiter("\\A"); String contents = scanner.hasNext() ? scanner.next() : "";
+        PrintWriter pw; try { pw = new PrintWriter(xmlPath); } catch (FileNotFoundException e) { throw new RuntimeException(e); }
+        pw.print(contents); pw.close();
+
+        // set the TextView's text to the output of the C++ parseXML() function
+        textView.setText(parseXML(xmlPath));
     }
 
     /**
      * A native method that is implemented by the 'myapplication' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native String parseXML(String xmlPath);
 }
